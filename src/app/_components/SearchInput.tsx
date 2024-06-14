08167d1a719a21b8bf4627ec347bdb9cd8ae8bac
pawfs <bahriayavx@gmail.com>
2024-06-14 20:03:11 +0100

    add SearchInput component with client-side search functionality

--- src/app/_components/SearchInput.tsx
+++ src/app/_components/SearchInput.tsx
@@ -1,22 +1,39 @@
 "use client";
 
+import { on } from "events";
 import { Search, X } from "lucide-react";
+import { useState } from "react";
 
 export default function SearchInput() {
+
+  // useState hook to manage the query state
+  const [query, setQuery] = useState("")
+
+
+  const executeSearchQuery = async () => {
+    // Sending the query as the request body to the /api/search endpoint
+    const response = await fetch("/api/search", {
+      method: "POST",
+      body: JSON.stringify({ query }),
+    });
+    const data = await response.json();
+    console.log(data.results);
+  }
+
   return (
     <div className="flex flex-row gap-4 rounded-lg border-2 border-white/10 py-4">
       <div className="flex flex-row rounded-lg border-2 border-black p-4">
         <input
-          className="h-full w-full focus:outline-none" type="text" onChange={() => { }}
+          className="h-full w-full focus:outline-none" type="text" placeholder="Search" value={query} onChange={(e) => setQuery(e.target.value)}
         />
-        <button onClick={() => { }}>
+        <button onClick={() => setQuery("")}>
           <span>
             <X />
           </span>
         </button>
       </div>
       <button
-        className="flex items-center justify-center gap-2 rounded-lg  p-4" onClick={() => { }}
+        className="flex items-center justify-center gap-2 rounded-lg  p-4" onClick={executeSearchQuery}
       >
         <span>
           <Search />
---
+++ src/app/api/search/route.ts
@@ -0,0 +1,35 @@
+import { env } from "process";
+
+type Payload = {
+  query: string;
+};
+
+export async function POST(request: Request) {
+  const payload = await request.json() as Payload;
+
+  if (!payload?.query) {
+    //return new Response("Query is required", { status: 400 });
+    return Response.json({ error: "No query provided" });
+  }
+
+  const response = await fetch(
+    `https://api.climatiq.io/data/v1/search?query=${payload.query}&data_version=12.12`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${env.CLIMATIQ_API_KEY}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+  if (!data) {
+    return Response.json({ error: "No results found" });
+  }
+
+  return Response.json(data);
+
+}
